21138557cbc1834016d2d6bb7cd96542d9a0a22a
PolGuixe <pol.guixe@gmail.com>
2014-12-08 00:40:18 +0000

    Removed insecure
    Removed insecure, and allowed certain writes to posts.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -5,6 +5,8 @@ Template.postSubmit.events({
     var post = {
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
-    }
+    };
+    post._id=Posts.insert(post);
+    Router.go('postPage',post);
   }
 });
